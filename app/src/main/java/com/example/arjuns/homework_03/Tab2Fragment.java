785da234225cdf3524507e66a3d1231fab1d0420
Arjun93 <nujra.an@gmail.com>
2015-07-07 03:27:23 -0400

    Added Camera feature. Image is yet to be retrieved and displayed in the gallery.

--- app/src/main/java/com/example/arjuns/homework_03/Tab2Fragment.java
+++ app/src/main/java/com/example/arjuns/homework_03/Tab2Fragment.java
@@ -1,10 +1,12 @@
 package com.example.arjuns.homework_03;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Created by arjuns on 7/2/2015.
@@ -14,6 +16,16 @@ public class Tab2Fragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.camera_layout, container, false);
+        Button myCaptureImageButton = (Button)view.findViewById(R.id.capturePhotoButton);
+        Button myCaptureVideoButton = (Button)view.findViewById(R.id.captureVideoButton);
+
+        myCaptureImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent invokeCameraIntent = new Intent(getActivity().getApplicationContext(),TakePictureActivity.class);
+                startActivity(invokeCameraIntent);
+            }
+        });
         return view;
     }
 }
